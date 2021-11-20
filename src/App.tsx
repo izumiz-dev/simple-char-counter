28e97b792f7f9c734b4b392961b2be17d43b63fd
Izumi Zama <zamaizumi@gmail.com>
2021-11-20 15:17:35 +0900

    add ability to count word in english

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { TextField } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import { ChangeEvent, useCallback, useState } from "react";
 import CSS from "csstype";
 
@@ -11,17 +13,44 @@ const style: CSS.Properties = {
 
 export const App = () => {
   const [text, setText] = useState("");
+  const [mode, setMode] = useState("char");
   const inputText = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value),
     []
   );
+
+  const handleChangeMode = (e: ChangeEvent<{}>, newMode: string | null) => {
+    setMode(newMode as string);
+  };
+
   return (
     <div style={style}>
-      <div style={{ fontWeight: "bold", fontSize: "4rem", padding: "1vh" }}>
-        {text.length || 0} chars
+      <div style={{ margin: "1vh", display: "flex", alignItems: "center" }}>
+        <span style={{ fontSize: "3rem", color: "#767676" }}>
+          {mode === "char"
+            ? text.length || 0
+            : text === ""
+            ? 0
+            : text.split(" ").length || 0}
+        </span>
+        <ToggleButtonGroup
+          style={{ margin: "1vh" }}
+          value={mode}
+          exclusive
+          onChange={handleChangeMode}
+          aria-label="text alignment"
+        >
+          <ToggleButton value="char" aria-label="char">
+            Char
+          </ToggleButton>
+          <ToggleButton value="word" aria-label="word">
+            Word (en)
+          </ToggleButton>
+        </ToggleButtonGroup>
       </div>
+
       <TextField
-        style={{ width: "75vw" }}
+        style={{ width: "75vw", margin: "1vh" }}
         id="outlined-basic"
         label="Text"
         variant="outlined"
